2b50e48e9b26559237494910a4287d7a0520fa3e
subin77 <subin.shahukhal@gmail.com>
2020-01-04 12:30:20 +0000

    intial commit

---
+++ src/main/java/com/solo/gurufirstapp/GurufirstappApplication.java
@@ -0,0 +1,13 @@
+package com.solo.gurufirstapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GurufirstappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GurufirstappApplication.class, args);
+	}
+
+}
